b1f17859ad5fa15df6e4e420a58a21cee446bf86
antonsouto <antonsouto97@gmail.com>
2026-04-16 20:41:15 +0200

    count_triplets_given_sum

--- src/two_pointers/count_triplets_given_sum.cpp
+++ src/two_pointers/count_triplets_given_sum.cpp
@@ -4,7 +4,33 @@ using namespace std;
 class Solution {
 public:
     int countTriplets(vector<int>& arr, int target) {
-        // tu solución aquí
-        return 0;
+        // Code Here
+        int i = 0;
+        int count = 0;
+        while(i < arr.size() - 2){
+            int left = i + 1;
+            int right = arr.size() - 1;
+            int complement = target - arr[i];
+            
+            while(left < right){
+                if(arr[left] + arr[right] == complement){
+                    count++;
+                    int aux=1;
+                    while((arr[right] == arr[right -aux]) && (left) != (right -aux))
+                    { 
+                        count++;
+                        aux++;
+                    }
+                    left++;
+                }else if((arr[left] + arr[right] > complement) ){
+                    right--;
+                }else{
+                    left++;
+                    right=arr.size() - 1;
+                }
+            }
+            i++;
+        }
+        return count;
     }
 };
